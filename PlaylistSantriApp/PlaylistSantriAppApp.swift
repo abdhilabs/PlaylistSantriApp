b2319ed323181dc1342fd627340a589444c52fee
Abdhilabs <rizaabdhi@gmail.com>
2021-02-19 10:56:08 +0700

    Initial Commit

---
+++ PlaylistSantriApp/PlaylistSantriAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistSantriAppApp.swift
+//  PlaylistSantriApp
+//
+//  Created by Abdhi on 19/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistSantriAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
